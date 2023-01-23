40102b0faac58da73be6f685c3fc4cb346044728
markzernin <86126066+markzernin@users.noreply.github.com>
2023-01-23 08:23:26 +0700

    Update functions.support.js

--- bot/common/support/functions.support.js
+++ bot/common/support/functions.support.js
@@ -2,6 +2,7 @@
 exports.getNextDate = async () => {
     let nextDay = null;
     let currentDate = new Date(); // текущая дата
+    currentDate.setHours(7, 0, 0);
 
     if (currentDate.getDay() === 0) {
         nextDay = new Date(new Date().setDate(currentDate.getDate() + 1)).toISOString().split('T')[0]; // если сегодня ВОСКРЕСЕНЬЕ
@@ -19,8 +20,8 @@ exports.getNextDate = async () => {
 // Функция будет вызываться, когда не получится отправить запрос на след день.
 // Функция будет возвращать либо текущий день, либо пятницу прошлой недели
 exports.getPreviousDate = async () => {
-
     let currentDate = new Date();
+    currentDate.setHours(7, 0, 0);
 
     let newDate = null;
 
@@ -35,6 +36,4 @@ exports.getPreviousDate = async () => {
     const [year, month, day] = newDate.split('-');
     newDate = `${day}.${month}.${year}`;
     return newDate;
-
 };
-
